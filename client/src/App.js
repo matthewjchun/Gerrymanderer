805a8725095ea51e9d0b852ce570d8e389e73ad6
Melanie Chio <melanie.chio@stonybrook.edu>
2021-09-21 23:03:03 -0400

    matt pls fix this LMAOO

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Map from './components/Map';
+
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 export default function App() {
+  let [viewport, setViewport] = useState({
+    latitude: 38.2749,
+    longitude: -98.35,
+    zoom: 2.8,
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
 
   return (
-  <div className="App">
-    <Map></Map>
-  </div>
+    <ReactMapGL
+      mapboxApiAccessToken={
+        "pk.eyJ1IjoibWpjaHVuIiwiYSI6ImNrdHV3Mmc5ZDIzd2cyb28ycjIxZHNkMjYifQ.60vjXo3tkAUMrL5aFgOkkQ"
+      }
+      {...viewport}
+      onViewportChange={(newView) => setViewport(newView)}
+    ></ReactMapGL>
   );
 }
