502f573528c7ee3b255e59565de731d21e5c49ac
Melanie Chio <melanie.chio@stonybrook.edu>
2021-09-22 00:53:09 -0400

    updated access token and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,28 @@
 import './App.css';
-// import Map from './components/Map';
 import TopBar from './components/TopBar';
 import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 export default function App() {
-  return (
-    <div className='App'>
-      <TopBar />
-      <Map></Map>
-    </div>
-  );
-}
-
-/*export default function App() {
   let [viewport, setViewport] = useState({
-    latitude: 38.2749,
-    longitude: -98.35,
-    zoom: 2.8,
+    latitude: 45,
+    longitude: -75,
     width: '100vw',
-    height: '93vh'
+    height: '100vh',
+    zoom: 11
   });
 
   return (
     <div>
       <TopBar />
       <ReactMapGL
-        mapboxApiAccessToken={
-          "pk.eyJ1IjoibWpjaHVuIiwiYSI6ImNrdHV3Mmc5ZDIzd2cyb28ycjIxZHNkMjYifQ.60vjXo3tkAUMrL5aFgOkkQ"
-        }
-        {...viewport}
-        // onViewportChange={(newView) => setViewport(newView)}
-      ></ReactMapGL>
+      {...viewport}
+      mapboxApiAccessToken = {'pk.eyJ1IjoiY2VsdGljczQxNiIsImEiOiJja3R2MGM5dTQxajY4Mm5sNWV5YnNhNHg0In0.t9oiLZZUeZi0QpqUIik13w'}
+      mapStyle="mapbox://styles/celtics416/cktv0fpu01ddn17nygi0ck49g"
+      onViewportChange={viewport => {
+        setViewport(viewport);}}
+      >
+      </ReactMapGL>
     </div>
   );
 }
-*/
