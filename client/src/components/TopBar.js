1087c340363c3c887939f39a28f55ab3570ee7cc
Samuel Ng <ngsamu4@gmail.com>
2021-09-27 14:20:27 -0400

    Buggy dropdown.

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -8,7 +8,7 @@ import LeftPane from './LeftPane';
 export default function TopBar(props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [activeState] = useContext(StateContext);
+  const [activeState, setActiveState] = useContext(StateContext);
 
   return (
     <HStack w='100%' p='5' align='center' justify='center'>
@@ -16,14 +16,20 @@ export default function TopBar(props) {
         <Text>Redistricting Settings</Text>
       </Button>
       <LeftPane isOpen={isOpen} onClose={onClose}></LeftPane>
-      <Text fontWeight='bold' fontSize='1.5em' m='auto'>
+      {/*<Text fontWeight='bold' fontSize='1.5em' m='auto'>
         {activeState}
-      </Text>
-      {/*<Select value={activeState} placeholder={activeState} onChange={handleStateSelect}>
+  </Text>*/}
+      <Select
+        value={activeState}
+        placeholder={activeState}
+        onChange={(e) => {
+          setActiveState(e.target.value);
+        }}
+      >
         <option value='Arizona'>Arizona</option>
         <option value='Michigan'>Michigan</option>
         <option value='Virginia'>Virginia</option>
-    </Select>*/}
+      </Select>
     </HStack>
   );
 }
