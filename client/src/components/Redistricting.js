3e03264d27e54a03d0361d81544a132480c95fed
Samuel Ng <ngsamu4@gmail.com>
2021-09-28 05:21:28 -0400

    Popover on hover for redistricting details.

--- client/src/components/Redistricting.js
+++ client/src/components/Redistricting.js
@@ -1,38 +1,58 @@
-import { Box, Text, Image, Tooltip } from '@chakra-ui/react';
+import { Box, Text, Image, VStack } from '@chakra-ui/react';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+} from '@chakra-ui/react';
 
 export default function Redistricting(props) {
   const { number, thumbnail, bestMeasure, measures } = props;
   const { popEquality, compactness, majorityMinority, enactedDeviation } =
     measures;
   return (
-    <Tooltip
-      label={`Population Equality: ${popEquality}
-      Compactness: ${compactness}
-      Majority-Minority: ${majorityMinority}
-      Deviation from Enacted Districting: ${enactedDeviation}`}
-    >
-      <Box
-        maxW='sm'
-        p='3'
-        borderWidth='1px'
-        borderRadius='lg'
-        overflow='hidden'
-        __hover={{ boxShadow: '0px 5px 8px #888888' }}
-      >
-        <Image maxW='100%' src={thumbnail} />
+    <Popover isLazy trigger='hover'>
+      <PopoverTrigger>
         <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          isTruncated
+          maxW='sm'
+          p='3'
+          borderWidth='1px'
+          borderRadius='lg'
+          overflow='hidden'
         >
-          <Text>District {number}</Text>
+          <Image maxW='100%' src={thumbnail} />
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            isTruncated
+          >
+            <Text>District {number}</Text>
+          </Box>
+          <Box maxW='100%' h='50px' overflow='hidden'>
+            <Text maxW='100%'>{bestMeasure}</Text>
+          </Box>
         </Box>
-        <Box maxW='100%' h='50px' overflow='hidden'>
-          <Text maxW='100%'>{bestMeasure}</Text>
-        </Box>
-      </Box>
-    </Tooltip>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverHeader fontWeight='semibold'>
+          Redistricting Details
+        </PopoverHeader>
+        <PopoverArrow />
+        <PopoverBody>
+          <VStack align='left'>
+            <Text>Population Equality: {popEquality}</Text>
+            <Text>Compactness: {compactness}</Text>
+            <Text>Majority-Minority: {majorityMinority}</Text>
+            <Text>Deviation from Enacted Districting: {enactedDeviation}</Text>
+          </VStack>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
   );
 }
