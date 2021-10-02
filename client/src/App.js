2c489900652234a46576a600cf344ac0a216b2fc
Samuel Ng <ngsamu4@gmail.com>
2021-10-02 18:15:41 -0400

    Prettier formatting.

--- client/src/App.js
+++ client/src/App.js
@@ -187,10 +187,17 @@ export default function App() {
     if (visibility === 'none') {
       map.current.setLayoutProperty('azcd_lines', 'visibility', 'visible');
     }
-    visibility = map.current.getLayoutProperty('azcounty-boundary', 'visibility');
+    visibility = map.current.getLayoutProperty(
+      'azcounty-boundary',
+      'visibility'
+    );
     if (visibility === 'none') {
-      map.current.setLayoutProperty("azcounty-boundary", "visibility", "visible");
-    };
+      map.current.setLayoutProperty(
+        'azcounty-boundary',
+        'visibility',
+        'visible'
+      );
+    }
   };
 
   const zoomMichigan = (map) => {
@@ -369,10 +376,17 @@ export default function App() {
     if (visibility === 'none') {
       map.current.setLayoutProperty('micd_lines', 'visibility', 'visible');
     }
-    visibility = map.current.getLayoutProperty('micounty-boundary', 'visibility');
+    visibility = map.current.getLayoutProperty(
+      'micounty-boundary',
+      'visibility'
+    );
     if (visibility === 'none') {
-      map.current.setLayoutProperty("micounty-boundary", "visibility", "visible");
-    };
+      map.current.setLayoutProperty(
+        'micounty-boundary',
+        'visibility',
+        'visible'
+      );
+    }
   };
 
   const zoomVirginia = (map) => {
@@ -519,10 +533,17 @@ export default function App() {
     if (visibility === 'none') {
       map.current.setLayoutProperty('vacd_lines', 'visibility', 'visible');
     }
-    visibility = map.current.getLayoutProperty('vacounty-boundary', 'visibility');
+    visibility = map.current.getLayoutProperty(
+      'vacounty-boundary',
+      'visibility'
+    );
     if (visibility === 'none') {
-      map.current.setLayoutProperty("vacounty-boundary", "visibility", "visible");
-    };
+      map.current.setLayoutProperty(
+        'vacounty-boundary',
+        'visibility',
+        'visible'
+      );
+    }
   };
 
   const zoomOut = (map) => {
@@ -546,8 +567,8 @@ export default function App() {
     if (visibility === 'visible') {
       hide();
     }
-    if (map.current.getLayer("azcounty-boundary") !== "undefined") {
-      map.current.setLayoutProperty("azcounty-boundary", "visibility", "none");
+    if (map.current.getLayer('azcounty-boundary') !== 'undefined') {
+      map.current.setLayoutProperty('azcounty-boundary', 'visibility', 'none');
     }
 
     // michigan
@@ -562,8 +583,8 @@ export default function App() {
     if (visibility === 'visible') {
       hide();
     }
-    if (map.current.getLayer("micounty-boundary") !== "undefined") {
-      map.current.setLayoutProperty("micounty-boundary", "visibility", "none");
+    if (map.current.getLayer('micounty-boundary') !== 'undefined') {
+      map.current.setLayoutProperty('micounty-boundary', 'visibility', 'none');
     }
 
     // virginia
@@ -578,8 +599,8 @@ export default function App() {
     if (visibility === 'visible') {
       hide();
     }
-    if (map.current.getLayer("vacounty-boundary") !== "undefined") {
-      map.current.setLayoutProperty("vacounty-boundary", "visibility", "none");
+    if (map.current.getLayer('vacounty-boundary') !== 'undefined') {
+      map.current.setLayoutProperty('vacounty-boundary', 'visibility', 'none');
     }
   };
 
@@ -797,119 +818,186 @@ export default function App() {
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
-    var district = document.getElementById("districtLegend");
-    var county = document.getElementById("countyLegend");
-    var precinct = document.getElementById("precinctLegend");
-    district.onclick = function(e) {  
+    var district = document.getElementById('districtLegend');
+    var county = document.getElementById('countyLegend');
+    var precinct = document.getElementById('precinctLegend');
+    district.onclick = function (e) {
       if (showDistrict) {
-        if (map.current.getLayer("azcd_lines") !== "undefined") {
-          map.current.setLayoutProperty("azcd_lines", "visibility", "none");
+        if (map.current.getLayer('azcd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('azcd_lines', 'visibility', 'none');
         }
-        if (map.current.getLayer("micd_lines") !== "undefined") {
-          map.current.setLayoutProperty("micd_lines", "visibility", "none");
+        if (map.current.getLayer('micd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('micd_lines', 'visibility', 'none');
         }
-        if (map.current.getLayer("vacd_lines") !== "undefined") {
-          map.current.setLayoutProperty("vacd_lines", "visibility", "none");
+        if (map.current.getLayer('vacd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('vacd_lines', 'visibility', 'none');
         }
       }
       if (!showDistrict) {
-        if (map.current.getLayer("azcd_lines") !== "undefined") {
-          map.current.setLayoutProperty("azcd_lines", "visibility", "visible");
+        if (map.current.getLayer('azcd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('azcd_lines', 'visibility', 'visible');
         }
-        if (map.current.getLayer("micd_lines") !== "undefined") {
-          map.current.setLayoutProperty("micd_lines", "visibility", "visible");
+        if (map.current.getLayer('micd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('micd_lines', 'visibility', 'visible');
         }
-        if (map.current.getLayer("vacd_lines") !== "undefined") {
-          map.current.setLayoutProperty("vacd_lines", "visibility", "visible");
+        if (map.current.getLayer('vacd_lines') !== 'undefined') {
+          map.current.setLayoutProperty('vacd_lines', 'visibility', 'visible');
         }
       }
-    }
+    };
     county.onclick = function (e) {
       if (showCounty) {
-        if (map.current.getLayer("azcounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("azcounty-boundary", "visibility", "none");
+        if (map.current.getLayer('azcounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'azcounty-boundary',
+            'visibility',
+            'none'
+          );
         }
-        if (map.current.getLayer("micounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("micounty-boundary", "visibility", "none");
+        if (map.current.getLayer('micounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'micounty-boundary',
+            'visibility',
+            'none'
+          );
         }
-        if (map.current.getLayer("vacounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("vacounty-boundary", "visibility", "none");
+        if (map.current.getLayer('vacounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'vacounty-boundary',
+            'visibility',
+            'none'
+          );
         }
       }
       if (!showCounty) {
-        if (map.current.getLayer("azcounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("azcounty-boundary", "visibility", "visible");
+        if (map.current.getLayer('azcounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'azcounty-boundary',
+            'visibility',
+            'visible'
+          );
         }
-        if (map.current.getLayer("micounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("micounty-boundary", "visibility", "visible");
+        if (map.current.getLayer('micounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'micounty-boundary',
+            'visibility',
+            'visible'
+          );
         }
-        if (map.current.getLayer("vacounty-boundary") !== "undefined") {
-          map.current.setLayoutProperty("vacounty-boundary", "visibility", "visible");
+        if (map.current.getLayer('vacounty-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'vacounty-boundary',
+            'visibility',
+            'visible'
+          );
         }
       }
-    }
+    };
     precinct.onclick = function (e) {
       if (showPrecinct) {
-        if (map.current.getLayer("azprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("azprec-boundary", "visibility", "none");
+        if (map.current.getLayer('azprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'azprec-boundary',
+            'visibility',
+            'none'
+          );
         }
-        if (map.current.getLayer("miprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("miprec-boundary", "visibility", "none");
+        if (map.current.getLayer('miprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'miprec-boundary',
+            'visibility',
+            'none'
+          );
         }
-        if (map.current.getLayer("vaprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("vaprec-boundary", "visibility", "none");
+        if (map.current.getLayer('vaprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'vaprec-boundary',
+            'visibility',
+            'none'
+          );
         }
       }
       if (!showPrecinct) {
-        if (map.current.getLayer("azprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("azprec-boundary", "visibility", "visible");
+        if (map.current.getLayer('azprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'azprec-boundary',
+            'visibility',
+            'visible'
+          );
         }
-        if (map.current.getLayer("miprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("miprec-boundary", "visibility", "visible");
+        if (map.current.getLayer('miprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'miprec-boundary',
+            'visibility',
+            'visible'
+          );
         }
-        if (map.current.getLayer("vaprec-boundary") !== "undefined") {
-          map.current.setLayoutProperty("vaprec-boundary", "visibility", "visible");
+        if (map.current.getLayer('vaprec-boundary') !== 'undefined') {
+          map.current.setLayoutProperty(
+            'vaprec-boundary',
+            'visibility',
+            'visible'
+          );
         }
       }
-    }
+    };
   });
 
   function toggleDistrict() {
     if (showDistrict) {
       showDistrict = false;
-      document.getElementById("districtColor").style.backgroundColor = "#FFFFFF";
+      document.getElementById('districtColor').style.backgroundColor =
+        '#FFFFFF';
     } else {
       showDistrict = true;
-      document.getElementById("districtColor").style.backgroundColor = "#45322f";
+      document.getElementById('districtColor').style.backgroundColor =
+        '#45322f';
     }
   }
   function toggleCounty() {
     if (showCounty) {
       showCounty = false;
-      document.getElementById("countyColor").style.backgroundColor = "#FFFFFF";
+      document.getElementById('countyColor').style.backgroundColor = '#FFFFFF';
     } else {
       showCounty = true;
-      document.getElementById("countyColor").style.backgroundColor = "#940f00";
+      document.getElementById('countyColor').style.backgroundColor = '#940f00';
     }
   }
   function togglePrecinct() {
     if (showPrecinct) {
       showPrecinct = false;
-      document.getElementById("precinctColor").style.backgroundColor = "#FFFFFF";
+      document.getElementById('precinctColor').style.backgroundColor =
+        '#FFFFFF';
     } else {
       showPrecinct = true;
-      document.getElementById("precinctColor").style.backgroundColor = "#ebd8d3";
+      document.getElementById('precinctColor').style.backgroundColor =
+        '#ebd8d3';
     }
   }
 
   return (
     <>
       <TopBar />
-      <div className="legend">
-        <h4 style={{fontWeight: "bold"}}>Legend</h4>
-        <div id="districtLegend" onClick={toggleDistrict}><span id="districtColor" style={{backgroundColor: "#45322f"}}></span>Districts</div>
-        <div id="countyLegend" onClick={toggleCounty}><span id="countyColor" style={{backgroundColor:  "#940f00"}}></span>Counties</div>
-        <div id="precinctLegend" onClick={togglePrecinct}><span id="precinctColor" style={{backgroundColor: "#ebd8d3"}}></span>Precincts</div>
+      <div className='legend'>
+        <h4 style={{ fontWeight: 'bold' }}>Legend</h4>
+        <div id='districtLegend' onClick={toggleDistrict}>
+          <span
+            id='districtColor'
+            style={{ backgroundColor: '#45322f' }}
+          ></span>
+          Districts
+        </div>
+        <div id='countyLegend' onClick={toggleCounty}>
+          <span id='countyColor' style={{ backgroundColor: '#940f00' }}></span>
+          Counties
+        </div>
+        <div id='precinctLegend' onClick={togglePrecinct}>
+          <span
+            id='precinctColor'
+            style={{ backgroundColor: '#ebd8d3' }}
+          ></span>
+          Precincts
+        </div>
       </div>
       <Flex className='content' direction='column' justify='center'>
         <div ref={mapContainer} className='mapContainer' />
